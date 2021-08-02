288e057e21146ad6181db4ab6c80d2d437a9e7ce
kevin zieber <zieber.kevinr@gmail.com>
2021-08-02 15:01:29 -0700

    refactoring, and loading es-build from unpkg instead of local now

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,15 +10,14 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         //Check to see if we've already fetched this required package
         //we're checking our local indexedDB in the browser to see if it's there
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
@@ -27,8 +26,43 @@ export const fetchPlugin = (inputCode: string) => {
 
         //if it is in the cache, return it immediately
         if (cachedResult) return cachedResult;
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         //otherwise, store the response in the local indexedDB cache
+        const { data, request } = await axios.get(args.path);
+
+        /*
+        Because esbuild doesn't support css libraries in the browser, we instead have to write this JS workaround:
+        Check to see if the filetype is css.
+          If so, create a style element and append it to the head of the document
+        Otherwise, contents is set equal to data that is returned from line 32, and passed in.
+        
+        The regular expressions escape characters that might trip up our workaround
+        Everything in the imported CSS library is then appended to the head
+        */
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+          `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
