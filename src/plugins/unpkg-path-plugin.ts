5b14cebccedf16453e2cc6a5af4a125bab534343
kevin zieber <zieber.kevinr@gmail.com>
2021-08-02 13:20:09 -0700

    dependencies now being cached in the browser indexedDB for better performance

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -37,13 +42,28 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
+
+        //Check to see if we've already fetched this required package
+        //we're checking our local indexedDB in the browser to see if it's there
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        //if it is in the cache, return it immediately
+        if (cachedResult) return cachedResult;
+
+        //otherwise, store the response in the local indexedDB cache
         const { data, request } = await axios.get(args.path);
-        // console.log(request);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
