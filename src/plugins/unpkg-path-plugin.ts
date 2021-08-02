8d8a0bcb9f22964175b4d37b8c7d850db29fb141
kevin zieber <zieber.kevinr@gmail.com>
2021-08-02 13:46:43 -0700

    modularized plugins for loading dependencies in the browser

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localForage from 'localforage';
 
-const fileCache = localForage.createInstance({
-  name: 'filecache',
-});
-
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -29,38 +23,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
           path: `https://unpkg.com/${args.path}`,
           namespace: 'a',
         };
-      });
-
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
-        //Check to see if we've already fetched this required package
-        //we're checking our local indexedDB in the browser to see if it's there
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        //if it is in the cache, return it immediately
-        if (cachedResult) return cachedResult;
-
-        //otherwise, store the response in the local indexedDB cache
-        const { data, request } = await axios.get(args.path);
-
-        const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
-        };
-
-        await fileCache.setItem(args.path, result);
-
-        return result;
       });
     },
   };
